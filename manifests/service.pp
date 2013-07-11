d9fcf9e23f7f2f82ca847b3ae8e585e2a14c2c84
Ilya Margolin <ilya@jimdo.com>
2013-07-11 16:06:44 +0200

    fix grepping of supervisorctl restart output to produce a sensible exit code
    also added `uniq` to check all processes in the process group (when
    num_procs > 1)

--- manifests/service.pp
+++ manifests/service.pp
@@ -91,7 +91,7 @@ define supervisor::service (
   service { "supervisor::${name}":
     ensure   => $service_ensure,
     provider => base,
-    restart  => "/usr/bin/supervisorctl restart ${process_name} | awk '/^${name}[:_]/{print \$2}' | grep -Pzo '^stopped\nstarted$'",
+    restart  => "/usr/bin/supervisorctl restart ${process_name} | awk '/^${name}[:_]/{print \$2}' | uniq | grep -z '^stopped.started.$'",
     start    => "/usr/bin/supervisorctl start ${process_name} | awk '/^${name}[:_]/{print \$2}' | grep '^started$'",
     status   => "/usr/bin/supervisorctl status | awk '/^${name}[:_]/{print \$2}' | grep '^RUNNING$'",
     stop     => "/usr/bin/supervisorctl stop ${process_name} | awk '/^${name}[:_]/{print \$2}' | grep '^stopped$'",
